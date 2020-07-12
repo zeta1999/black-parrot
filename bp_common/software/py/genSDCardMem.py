b5094d273a014d3d6fedfa768f5e981ca020ed52
sripathi-muralitharan <sripathimuralitharan@gmail.com>
2020-07-11 17:19:07 -0700

    Renaming and moving SD Card memory generator script

--- bp_common/software/py/genSDCardMem.py
+++ bp_common/software/py/genSDCardMem.py
@@ -4,7 +4,7 @@ import os
 parser = ArgumentParser(description='CCE Microcode 34-bit to 64-bit converter')
 parser.add_argument('-i', dest='in_file', type=str, default=None,
                     help='Input memory file (.mem)', required=True)
-parser.add_argument('-o', dest='out_file', type=str, default='./temp.bin',
+parser.add_argument('-o', dest='out_file', type=str, default='./sd_card.mem',
                     help='Output binary file')
 
 args = parser.parse_args()
@@ -14,6 +14,7 @@ with open(os.path.abspath(args.in_file), 'r') as rf:
     lines = rf.readlines() 
     for line in lines:
       wf.write(format(int(line, 2), 'X').zfill(16))
+      wf.write("\n")
 
 rf.close()
 wf.close()
